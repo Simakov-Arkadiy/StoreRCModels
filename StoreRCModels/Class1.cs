559b71e76675fa0e033c628778babd390d62524d
Simakov-Arkadiy <simakovarkadii@mail.ru>
2025-10-27 20:59:51 +0300

    Add project files.

---
+++ StoreRCModels/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace StoreRCModels
+{
+    public abstract class RCModel
+    {
+        public int numberRCModdel { get; }
+        public string nameRCModel { get; }
+        public string typeRCModel { get; }
+
+        public RCModel(int numberRCModel, string nameRCmodel, string typeRCModel) 
+        {
+            this.numberRCModdel = numberRCModel;
+            this.nameRCModel = nameRCmodel;
+            this.typeRCModel = typeRCModel;
+        }
+        
+    }
+}
